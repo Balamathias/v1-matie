6fcbc6390c6e7cbfb67daa431286a2617aef8385
Balamathias <balamathias05@gmail.com>
2023-11-22 01:24:57 +0100

    v 1.0.2

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`inter.className common-body`}>{children}</body>
+      <body className={`${inter.className} common-body`}>{children}</body>
     </html>
   )
 }
