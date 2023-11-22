a0fee83fa228bc04d332c09f9ce9afd05bab0c01
Balamathias <balamathias05@gmail.com>
2023-11-22 01:01:42 +0100

    new commit

--- frontend/client/client.ts
+++ frontend/client/client.ts
@@ -2,7 +2,7 @@ import {createClient, type ClientConfig} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 const config: ClientConfig = {
-  projectId: process.env.NEXT_SANITY_PROJECT_ID,
+  projectId: 'a8ep10pk',
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2022-03-07', // use current date (YYYY-MM-DD) to target the latest API version
